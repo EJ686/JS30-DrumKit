90da4b5da350f42a3d5b3c42098c34ff795dd61e
Edgar Juarez <ejjt.686@gmail.com>
2017-06-05 19:25:31 -0700

    Added audio playing function when key is pressed

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
     if (!audio) return; // Stops the function from running all together
-    console.log(audio);
+    audio.currentTime = 0; // Rewinds to the start of audio file
+    audio.play();
 });
