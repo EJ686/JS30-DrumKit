d50815472cef2b87d0493dddad65a09467fa53a5
Edgar Juarez <ejjt.686@gmail.com>
2017-06-05 19:16:44 -0700

    Added an if statement to stop script from running when unwanted keys are pressed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Event listener
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    if (!audio) return; // Stops the function from running all together
     console.log(audio);
 });
