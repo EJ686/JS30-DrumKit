3fbaad4caa79bc5ec4de81339225ca2492ddcfba
Edgar Juarez <ejjt.686@gmail.com>
2017-06-05 19:48:01 -0700

    Pressing key adds and remove css style transitions

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-// Event listener
+// Keypress events
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     if (!audio) return; // Stops the function from running all together
     audio.currentTime = 0; // Rewinds to the start of audio file
     audio.play();
+    key.classList.add('playing');
 });
+
+
+// Transition effects
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
